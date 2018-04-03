5162b0d19f119604e3d3178b26caf2da1da0f60b
dorukhanakar <dorukhanakar@gmail.com>
2018-04-04 02:52:22 +0300

    Fix Push Notification Bug
    Signed-off-by: dorukhanakar <dorukhanakar@gmail.com>

--- app/src/main/java/dakar/miniapp/AppsActivity.kt
+++ app/src/main/java/dakar/miniapp/AppsActivity.kt
@@ -1,12 +1,12 @@
 package dakar.miniapp
 
+import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_apps.*
 import java.util.*
@@ -56,17 +56,19 @@ class AppsActivity : AppCompatActivity() {
         }
         btn_pushntf.setOnClickListener {
 
-            val pendingIntent = PendingIntent.getActivity(this@AppsActivity, 0, intent, 0)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
-            val notification = NotificationCompat.Builder(this)
+            val notification = Notification.Builder(this)
                     .setTicker("")
                     .setContentTitle("Notification")
                     .setContentText("Merhaba")
+                    .setSmallIcon(R.drawable.notification_icon_background)
                     .setContentIntent(pendingIntent).notification
 
-
+            notification.flags = Notification.FLAG_AUTO_CANCEL
             val notificationmanager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationmanager.notify(0, notification)
+
 
         }
     }
